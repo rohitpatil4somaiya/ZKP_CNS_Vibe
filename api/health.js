65c673a425ff2e5fc64e7d2f5b25cde1f363930c
Rohit1034 <138391793+Rohit1034@users.noreply.github.com>
2025-11-09 18:03:13 +0530

    health checkup

---
+++ api/health.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  if (req.method === 'OPTIONS') return res.status(204).end()
+  res.json({ status: 'ok' })
+}
